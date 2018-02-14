c5b4ed7d4a8687eb3cb86e9a740b14fd240f3c51
AnjaAF4 <you@example.com>
2018-02-14 16:04:55 +0100

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'WELCOME TO ANNE & ANJA BLOG'
+    return render_template('pages/home.html')
+
+@app.route('/about')
+def about():
+    return render_template('pages/about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('pages/contact.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
